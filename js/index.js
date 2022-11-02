0a9801184167a62fe77be47ff9adc850426129d3
kseniasadrina22 <kseniasadrina212@gmail.com>
2022-11-02 13:12:51 +0200

    Add Part2

--- js/index.js
+++ js/index.js
@@ -42,22 +42,27 @@ console.log("numberVariable2", numberVariable2);
 let num = 1;
 num += 11;
 console.log("num :>>", num);
+
 //num = num – 11;
 let num1 = 1;
 num1 -= 11;
 console.log("num1 :>>", num1);
+
 //num = num * 11;
 let num2 = 1;
 num2 *= 11;
 console.log("num2 :>>", num2);
+
 //num = num / 11;
 let num3 = 1;
 num3 /= 11;
 console.log("num3 :>>", num3);
+
 //num = num + 1;
 let num4 = 1;
 num4 += 1;
 console.log("num4 :>>", num4);
+
 //num = num – 1;
 let num5 = 1;
 num5 -= 1;
@@ -68,9 +73,20 @@ console.log("num5 :>>", num5);
 //Таски на prompt (вводити з допомогою prompt, виводити з допомогою alert):
 
 //1) Запитати число у користувача, піднести його до квадрату і вивести результат.
+const inputValue = Number(prompt("Enter a number:"));
+const alert = inputValue ** 2;
+console.log("alert :>> ", alert);
 
 //2) Вивести середнє арифметичне двох чисел. Числа запитувати у користувача.
-
-//3) Перевести кількість хвилин на секунди та вивести результат. Число хвилин запитувати у користувача.
-
+const aa = +prompt("input value");
+const bb = Number(prompt("input value"));
+const results = (a + b) / 2;
+console.log("Yuor result is ");
+//3) Перевести кількість хвилин на секунди та вивести 'результат. Число хвилин запитувати у користувача.
+const a1 = Number(prompt("input value"));
+const result = a * 60;
+console.log("Your result is");
 //4) Створіть змінну greeting зі значенням 'Hello,' і оголосіть змінну userName. Запитайте ім'я користувача та внесіть це значення у змінну userName. Виведіть повідомлення з привітанням, наприклад, 'Hello, Vasya!'. (згадайте про конкатенацію)
+let userName = prompt("input value");
+const greeting = "Hello + userName )!";
+console.log("greeting :>>", greeting);
